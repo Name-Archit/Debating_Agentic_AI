05091b0718d0faaba202075990fe4627db663c21
ArchitNiranjan <kazutikirito5665@gmail.com>
2026-02-25 17:20:00 +0530

    added response from AI

--- Main.py
+++ Main.py
@@ -3,7 +3,33 @@ from typing import override
 from groq import Groq
 from dotenv import load_dotenv
 import json
-from IPython.display import Markdown, display
+from rich.console import Console
+from rich.markdown import Markdown
+from prompt import Archit_Prompt
+from prompt import Alter_Archit_Prompt
+from prompt import Topic
 
 load_dotenv(override = True)
 grok_key = os.getenv("GROQ_API_KEY")
+
+Ego = Archit_Prompt
+Alter_ego = Alter_Archit_Prompt
+Topic = Topic
+groq = Groq()
+console = Console()
+
+Ego_message =[
+    {"role": "system", "content": Ego},
+    {"role": "user", "content": f"""Topic = {Topic} 
+    make your output very precise and to the point maybe under 3-4 lines dont use 
+    heavy words make it simple, easy and clean."""}
+]
+
+Ego_response = groq.chat.completions.create(
+    model = "openai/gpt-oss-20b",
+    messages = Ego_message
+)
+
+Ego_response1 = Ego_response.choices[0].message.content
+console.print(Markdown(Ego_response1))
+
---
+++ prompt.py
@@ -0,0 +1,9 @@
+
+Archit_Prompt = """Assume yourself as optimistic viewer and see situation through emotion you are introvert and like to enjoy arts, comfort and easy lifestyle.
+you like to work enough that let only your tomorrow easy, you dont look far in the future that much. """
+
+Alter_Archit_Prompt = """Assume yourself as semmistic viewer you are an ambivert and like to enjoy money and discomfrot because that makes you feel like you are growing
+and becoming strong you are more logical reasoner not emotional one, you enjoy struggle and try to look far in the future so that you should have plan on how to tackle the problems. """
+
+Topic = """Is it okay to spend 7 hours of your day for comfort because it do make you happy but 7 hours feels too much to spent and another 8 hours in sleep then you are left with
+9-10 hour of working time. So am i doing good because in the end i am happy in that 7 hour of a day?"""

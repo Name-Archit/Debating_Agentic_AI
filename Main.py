1a54b9797bee621c94afb13a877e4fe57cc10ca1
ArchitNiranjan <kazutikirito5665@gmail.com>
2026-02-27 20:00:41 +0530

    added function for ego & alter ego, removed their distinct file

--- Main.py
+++ Main.py
@@ -5,27 +5,116 @@ from dotenv import load_dotenv
 import json
 from rich.console import Console
 from rich.markdown import Markdown
-from prompt import Archit_Prompt
-from prompt import Alter_Archit_Prompt
-from prompt import Topic
-from Ego import Ego_response
-from alterEgo import alterEgo_response
+from prompt import Archit_Prompt, Alter_Archit_Prompt, Topic, JudgePrompt
 
 load_dotenv(override = True)
 grok_key = os.getenv("GROQ_API_KEY")
 
 Ego = Archit_Prompt
 Alter_ego = Alter_Archit_Prompt
+judge = JudgePrompt
+
 groq = Groq()
 console = Console()
 
 Debate = []
 
-Ego_response1 = Ego_response.choices[0].message.content
-Debate.append({"role": "assistant", "content": Ego_response1})
+flag = 0
 
-alterEgoResponse1 = alterEgo_response.choices[0].message.content
-Debate.append({"role": "assistant", "content": alterEgoResponse1})
+def architAlter(term, word):
+    if word == "alter":
+        alterEgo_message = [
+        {"role": "system", "content": Alter_ego},
+        {"role": "user", "content": f"""Topic = {Topic} 
+        response = {Debate[0]}
+        Give your own opinion on why the response is in your opinion is not correct for the topic and make your opinion
+        precise and easy to understand, opinion should be maximum 3-4 lines dont use heavy words and keep it simple, easy and clean."""}
+        ]
 
-for message in Debate:
-    print(message["content"])
+        alterEgo_response = groq.chat.completions.create(
+        model = "openai/gpt-oss-20b",
+        messages = alterEgo_message
+        )
+
+        alterEgoResponse1 = alterEgo_response.choices[0].message.content
+        Debate.append({"role": "assistant", "content": alterEgoResponse1})
+
+    elif word == "alterDebate":        
+
+        alterEgo_message_Debate = [
+        {"role": "system", "content": Alter_ego},
+        {"role": "user", "content": f"""Topic = {Topic} 
+        response = {Debate[term]}
+        Give your own opinion on why the response is in your opinion is not correct for the topic and make your opinion
+        precise and easy to understand, opinion should be maximum 3-4 lines dont use heavy words and keep it simple, easy and clean."""}
+        ]
+
+        alterEgo_response_Debate = groq.chat.completions.create(
+        model = "openai/gpt-oss-20b",
+        messages = alterEgo_message_Debate
+        )
+
+        alterEgoResponses = alterEgo_response_Debate.choices[0].message.content
+        Debate.append({"role": "assistant", "content": alterEgoResponses})
+
+def architEgo(term, word):
+
+    if word == "ego":
+        Ego_message =[
+        {"role": "system", "content": Ego},
+        {"role": "user", "content": f"""Topic = {Topic} 
+        make your output very precise and to the point maybe under 3-4 lines dont use 
+        heavy words make it simple, easy and clean."""}
+        ]
+
+        Ego_response = groq.chat.completions.create(
+        model = "openai/gpt-oss-20b",
+        messages = Ego_message
+        )
+
+        Ego_response1 = Ego_response.choices[0].message.content
+        Debate.append({"role": "assistant", "content": Ego_response1})
+
+    elif word == "egoDebate":
+        Ego_message_Debate =[
+        {"role": "system", "content": Ego},
+        {"role": "user", "content": f"""Topic = {Topic} 
+        response = {Debate[term]}
+        make your output very precise and to the point maybe under 3-4 lines dont use 
+        heavy words make it simple, easy and clean."""}
+        ]
+
+        Ego_response_Debate = groq.chat.completions.create(
+        model = "openai/gpt-oss-20b",
+        messages = Ego_message_Debate
+        )
+
+        Ego_responses = Ego_response_Debate.choices[0].message.content
+        Debate.append({"role": "assistant", "content": Ego_responses})
+
+
+print("How many round you want the debate to happen: ")
+n = int(input())
+
+term = 0
+architEgo(term, "ego")
+architAlter(term, "alter")
+
+n -= 1
+
+while n != 0 :
+    if flag == 0:
+        term += 1
+        architEgo(term, "egoDebate")
+        flag = 1
+
+    else:
+        term += 1
+        architAlter(term, "alterDebate")
+        flag = 0
+
+    n-=1
+
+
+
+
